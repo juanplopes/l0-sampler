2edf447cebd8d6a9533aa4926c4b0c5175df0f27
Juan Lopes <me@juanlopes.net>
2018-04-06 07:16:51 -0300

    X

--- findsin.py
+++ findsin.py
@@ -1,16 +1,22 @@
 import math
 
 def f(q):
-    m = 32
+    m = 64
     return reduce(lambda a,b: a*b, (1 - q*2**i * math.exp(-q*2**i) for i in range(-m, m+1)))
 
 V = [(f(1+x/1e5), 1+x/1e5) for x in range(int(1e5+1))]
 answer1 = (min(V)[0]+max(V)[0])/2
 
-print min(V), max(V), answer1
+
     
 answer2 = filter(lambda (y, x): (abs(y-answer1) < 5e-11), V)[0][1]
+answer2 = math.pi * 2 * math.log(answer2, 2)
 
-print answer2, math.pi * 2 * math.log(answer2, 2)
 
-answer3 = 
+
+answer3 = (V[0][0]-answer1)/math.sin(2*math.pi*math.log(1, 2)-answer2)
+
+print '%.12f' % answer1
+print '%.12f' % answer3
+print '%.12f' % answer2
+
---
+++ test3.py
@@ -0,0 +1,10 @@
+import math
+
+def f(m, r):
+    return reduce(lambda a,b: a*b, (1 - float(r)/2**i * (1-1.0/2**i)**(r-1) for i in range(1, m+1)))
+    
+def g(m, r):
+    return reduce(lambda a,b: a*b, (1 - float(r)/2**i * math.exp(-float(r)/2**i) for i in range(1, m+1)))
+    
+for r in range(0, 2**20, 2**20/128):
+    print r, '\t', f(1000, r), '\t', g(1000, r)
