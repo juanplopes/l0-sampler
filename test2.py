4b19d93ddfc4adcc1c7b028f92850522034b97ab
Juan Lopes <me@juanlopes.net>
2018-04-24 23:45:04 -0300

    tests

--- test2.py
+++ test2.py
@@ -1,9 +1,18 @@
 import math
 
+def exp(x):
+    try:
+        return math.exp(x)
+    except:
+        return float('inf')
+
 def f(q, m):
-    return reduce(lambda a,b: a*b, (1 - q*2**i * math.exp(-q*2**i) for i in range(-m, m+1)))
-    
-x = 1
-while x < 2:
-    print x, '\t', f(x, 5)
+    return reduce(lambda a,b: a+b, (math.log(1 - 2**(i+q) * math.exp(-2**(i+q))) for i in range(-m, m+1)))
+   
+def flog(q, m):
+    return reduce(lambda a,b: a+b, ((2**(2*(q+i)) - 2**(q+i))/(exp(2**(q+i)) - 2**(q+i)) for i in range(-m, m+1)))
+
+x = 0
+while x < 4:
+    print x, '\t', f(x, 30), '\t', flog(x, 30)
     x += 0.01
--- test3.py
+++ test3.py
@@ -6,5 +6,5 @@ def f(m, r):
 def g(m, r):
     return reduce(lambda a,b: a*b, (1 - float(r)/2**i * math.exp(-float(r)/2**i) for i in range(1, m+1)))
     
-for r in range(0, 2**20, 2**20/128):
+for r in range(0, 2**16, 2**16/128):
     print r, '\t', f(1000, r), '\t', g(1000, r)
